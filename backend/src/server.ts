d8cae6d7254e6dd8b8ed3e81f3628cb21f110b1e
Leandro Ferreira <leanfj@hotmail.com>
2020-10-14 00:11:22 -0300

    Criando tabelas, rotas, controllers e inserindo na tabela

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,10 @@
 import express from 'express'
+import './database/connection'
+
+import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
-
-app.get('/users', (request, response) => {
-
-  return response.json({message: 'hello world!!'})
-
-})
-
+app.use(routes)
 app.listen(3333)
