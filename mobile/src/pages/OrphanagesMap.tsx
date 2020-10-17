672ec5eef10bfc9b067c62496f7ced0497e9b8ee
Leandro Ferreira <leanfj@hotmail.com>
2020-10-17 18:29:47 -0300

    Criado fluxo de navegação das telas e headers

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps'
 import { Feather } from '@expo/vector-icons'
 import mapMarker from '../images/map-marker.png'
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 export default function OrphanagesMap() {
   
   const navigation = useNavigation()
@@ -11,7 +12,9 @@ export default function OrphanagesMap() {
   function handleNavigateOrphanageDetails() {
     navigation.navigate('OrphanageDetails')
   }
-  
+  function handleNavigateCreateOrphanage () {
+    navigation.navigate('SelectMapPosition')
+  }  
   return (
     <View style={styles.container}>
       <MapView
@@ -42,9 +45,9 @@ export default function OrphanagesMap() {
 
       <View style={styles.footer}>
         <Text style={styles.footerText}>2 Orfanatos encontrados</Text>
-        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => { }}>
+        <RectButton style={styles.createOrphanageButton} onPress={handleNavigateCreateOrphanage}>
           <Feather name="plus" size={20} color="#fff" ></Feather>
-        </TouchableOpacity>
+        </RectButton>
       </View>
     </View>
   )
