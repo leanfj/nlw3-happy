bc099e9b89ebdc1d203454cdf8fe6912a0986159
Leandro Ferreira <leanfj@hotmail.com>
2020-10-19 20:11:12 -0300

    Incluindo button para tela login

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -10,17 +10,23 @@ function Landing() {
 
     <div id="page-landing" className="animate__animated animate__fadeIn">
       <div className="content-wrapper">
-        <img src={logoImg} alt="Logo" className="animate__animated animate__backInLeft" />
+        <div className="image-container">
+
+          <img src={logoImg} alt="Logo" className="animate__animated animate__backInLeft" />
+          <div className="location">
+            <strong>Rio de Janeiro</strong>
+            <span>RJ</span>
+          </div>
+        </div>
         <main>
           <h1 className="animate__animated animate__fadeInDown">Leve felicidade para o mundo</h1>
           <p className=" animate__animated animate__fadeInUp">Visite orfanatose mude o dia de muitas crianças.</p>
         </main>
-        <div className="location">
-          <strong>Rio de Janeiro</strong>
-          <span>RJ</span>
-        </div>
         <Link to="/app" className="enter-app">
           <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
+        </Link>
+        <Link to="/login" className="button-login">
+          Área restrita
         </Link>
       </div>
     </div>
